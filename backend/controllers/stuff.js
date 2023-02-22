2257834bd5b14741874ab4f967aad8a2465227f8
Sylvain <sy.caparros@laposte.net>
2023-02-22 19:17:53 +0100

    modification de l_image d_une sauce

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -42,15 +42,19 @@ exports.modifyThing = (req, res, next) => {
     ...JSON.parse(req.body.thing),
     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
 } : { ...req.body };
-
 delete thingObject._userId;
 Thing.findOne({_id: req.params.id})
     .then((thing) => {
         if (thing.userId != req.auth.userId) {
             res.status(401).json({ message : 'Not authorized'});
         } else {
-            Thing.updateOne({ _id: req.params.id}, { ...thingObject, _id: req.params.id})
-            .then(() => res.status(200).json({message : 'Objet modifié!'}))
+          if (req.file) {
+            const filename = sauce.imageUrl.split('/images/')[1];
+            console.log(filename);
+            fs.unlink(`images/${filename}`, (err) => { console.log(err) });
+        }
+        Thing.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+            .then(() => res.status(200).json({ message: 'Sauce modifié!' }))
             .catch(error => res.status(401).json({ error }));
         }
     })
